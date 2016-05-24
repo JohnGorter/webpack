22cc35b700f25dda2c5fce4d73292aa62abf1c61
John Gorter <johngorter@iMac-van-John.local>
2016-05-24 23:10:10 +0200

    test

---
+++ printhelp.js
@@ -0,0 +1,7 @@
+console.log('\033c');
+console.log("Training webpack");
+console.log("Info Support BV");
+console.log(" - \"npm start\" to start presentation");
+console.log(" - \"npm run generatelabs\" to generate pdf files for the labs");
+console.log("");
+console.log("");
