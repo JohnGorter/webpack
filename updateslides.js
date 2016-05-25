93562c793026485e53a9b593fc089afb995db54d
john gorter <john.gorter@gmail.com>
2016-05-25 13:25:38 +0200

    'testingabc'

--- updateslides.js
+++ updateslides.js
@@ -15,38 +15,10 @@ console.log('pushing repository to github...');
 result =  spawn('git', ['push'], options);
 console.log(result.output[1].toString()); 
 console.log(result.output[2].toString()); 
+result =  spawn('git', ['subtree', '--prefix', '_slides', 'push', 'origin', 'gh-pages'], options);
+console.log(result.output[1].toString()); 
+console.log(result.output[2].toString()); 
 console.log('Done.');
 
  
   
-// startAndWait('git', ['add','.'])
-// .then(() => {return startAndWait('git', ['commit','-am\'testing\'']);})  
-// .then(() => {return startAndWait('git', ['push','.'])});
-
-   
-function startAndWait(process, args) {
-    return new Promise((resolve, reject) =>{
-         console.log('starting process...');
-        const p = spawn(process, args);
-       
-        p.stdin.on('readable', () => {
-            var chunk = process.stdin.read();
-            if (chunk !== null) {
-                 console.log(`data: ${chunk}`);
-            }
-          });
-
-
-        p.stderr.on('readable', () => {
-            var chunk = process.stdin.read();
-            if (chunk !== null) {
-                 console.log(`data: ${chunk}`);
-            }
-          });
-
-        p.on('close', (code)=>{
-            console.log(`exit code ${code}`);
-            resolve(code);
-        });
-    });
-}
