995a70bfcc2ca8817c9a41a7c89cc0aadc2f3ca4
john gorter <john.gorter@gmail.com>
2016-05-25 12:47:58 +0200

    'testing'

--- updateslides.js
+++ updateslides.js
@@ -1,30 +1,29 @@
 var spawn = require('child_process').spawn;
-const add = spawn('git', ['add','.']);
-const commit = spawn('git', ['commit','-am\'testing\'']);
-const push = spawn('git', ['push','.']);
  
 
-startAndWait(add)
-.then(() => {return startAndWait(commit);})  
-.then(() => {return startAndWait(push)});
+startAndWait('git', ['add','.'])
+.then(() => {return startAndWait('git', ['commit','-am\'testing\'']);})  
+.then(() => {return startAndWait('git', ['push','.'])});
 
 
-function startAndWait(process) {
+function startAndWait(process, args) {
     return new Promise((resolve, reject) =>{
-        console.log('starting process...')
-       process.stdin.on('readable', () => {
+         console.log('starting process...');
+        const p = spawn(process, args);
+       
+        p.stdin.on('readable', () => {
             var chunk = process.stdin.read();
             if (chunk !== null) {
                  console.log(`data: ${chunk}`);
             }
           });
 
-        process.stderr.on('data', (data) => {
+        p.stderr.on('data', (data) => {
             console.log(`${data}`);
             reject(); 
         });
 
-        process.on('close', (code)=>{
+        p.on('close', (code)=>{
             console.log(`exit code ${code}`);
             resolve(code);
         });
