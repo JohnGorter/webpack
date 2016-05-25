2bce432c4998fac534f9969db9be6ce6a3ccc8f3
john gorter <john.gorter@gmail.com>
2016-05-25 13:23:04 +0200

    'testing'

--- updateslides.js
+++ updateslides.js
@@ -6,12 +6,15 @@ var options = {
 console.log('adding files.....');
 var result = spawn('git', ['add','.'], options);
 console.log(result.output[1].toString());
+console.log(result.output[2].toString());
 console.log('commiting files...');
 result =  spawn('git', ['commit','-am\'testing\''], options);
 console.log(result.output[1].toString());
+console.log(result.output[2].toString());
 console.log('pushing repository to github...');
 result =  spawn('git', ['push'], options);
 console.log(result.output[1].toString()); 
+console.log(result.output[2].toString()); 
 console.log('Done.');
 
  
