5c944ae8c97e89dd791c431dd62a4d47dc334226
john gorter <john.gorter@gmail.com>
2016-05-25 12:32:09 +0200

    'test'

--- updateslides.js
+++ updateslides.js
@@ -1,10 +1,12 @@
 var spawn = require('child_process').spawn;
 const add = spawn('git', ['add','.']);
 const commit = spawn('git', ['commit','-am\'test\'']);
-// const add = spawn('git', ['add','.']);
+const push = spawn('git', ['push','.']);
  
-console.log('starting add.')
-startAndWait(add).then(() => {startAndWait(commit)});
+
+startAndWait(add)
+.then(() => {startAndWait(commit)})
+.then(() => {startAndWait(push)});
 
 function startAndWait(process) {
     return new Promise((resolve, reject) =>{
